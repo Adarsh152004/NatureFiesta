4e93fc13bee5f9ef6d0b3eb261ca708e746db3a1
Hrishee-das <prajapati.hrishee.4@gmail.com>
2025-03-24 19:05:03 +0530

    Handled click event on category card

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,22 @@
 console.log('entered in script');
+
 document.addEventListener('DOMContentLoaded', function () {
   const productCards = document.querySelectorAll('.main-content-product-card');
+  const categoryCards = document.querySelectorAll(
+    '.main-content-categories-card'
+  );
 
   productCards.forEach((card) => {
     card.addEventListener('click', function () {
       const productId = this.getAttribute('data-id'); // Get the product ID
       window.location.href = `/product/${productId}`; // Redirect to the product page
+    });
+  });
+
+  categoryCards.forEach((card) => {
+    card.addEventListener('click', function () {
+      const categoryId = this.getAttribute('data-id');
+      window.location.href = `/categories/${categoryId}`;
     });
   });
 });
