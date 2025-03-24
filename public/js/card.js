e8328ccf530696eab4e4ce6a427a2ee04d593c94
Hrishee-das <prajapati.hrishee.4@gmail.com>
2025-03-24 23:59:21 +0530

    changes in js

--- public/js/card.js
+++ public/js/card.js
@@ -1,24 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const productCards = document.querySelectorAll('.main-content-product-card');
-  const categoryCards = document.querySelectorAll(
-    '.main-content-categories-card'
-  );
-
-  productCards.forEach((card) => {
-    card.addEventListener('click', function () {
-      const productId = this.getAttribute('data-id'); // Get the product ID
-      window.location.href = `/product/${productId}`; // Redirect to the product page
-    });
-  });
-
-  categoryCards.forEach((card) => {
-    card.addEventListener('click', function () {
-      const categoryId = this.getAttribute('data-id');
-      window.location.href = `/categories/${categoryId}`;
-    });
-  });
-});
-
 let products = [];
 
 function changeImage(element) {
