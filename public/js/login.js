b8870d2e083d296173505b3674b203e2d43f8321
Adarsh152004 <krishnap8122@gmail.com>
2025-03-29 21:01:55 +0530

    fixed

--- public/js/login.js
+++ public/js/login.js
@@ -31,7 +31,7 @@ export const logout = async () => {
     });
 
     if (res.data.status === 'success') {
-      location.reload(true);
+      window.location.replace('/');
     }
   } catch (err) {
     alert('error', 'Error logging out! Try again.');
